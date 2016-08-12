2bd86cf7a3c634d8d45e236779172c951c041e9e
youngkook <youngkook1992@163.com>
2016-08-12 11:19:57 +0800

    Initial Commit

---
+++ LearnCLanguage/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LearnCLanguage
+//
+//  Created by Youngkook on 16/8/12.
+//  Copyright © 2016年 youngkook. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
